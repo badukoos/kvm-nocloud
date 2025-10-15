4040904727145511f396c069b5d5471f59a32d99
Arun Gopinath <arun.gopinath2@outlook.com>
2025-10-15 07:38:01 +0000

    force pre-reqs

--- vm.sh
+++ vm.sh
@@ -9,7 +9,12 @@ error() {
   printf "\033[1;31mERROR:\033[0m %s\n" "$*" >&2
   exit 1
 }
-
-info "foo"
-warn "foo"
-error "foo"
+requires() {
+  if ! command -v "$1" >/dev/null 2>&1; then
+    echo "missing tool: $1" >&2
+    exit 1
+  fi
+}
+for t in python3 curl sha256sum sha512sum virsh virt-install cloud-localds; do
+  requires "$t"
+done
